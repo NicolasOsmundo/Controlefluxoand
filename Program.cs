c9e4c0015c67c7b51dce5ef767119c32603c8a12
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 08:35:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Controlefluxoand
+{
+	static void Main(string[] args)
+	{
+		int idade = 20;
+
+		if (idade > 15 &&  idade < 18)
+	{
+			Console.WriteLine("SUB-17");
+		}
+	else if (idade > 17 && idade < 21)
+	{
+			Console.WriteLine("SUB-20");
+		}
+	else if (idade > 21 && idade < 24)
+	{
+			Console.WriteLine("SUB-23");
+		}
+		Console.Read();
+	}
+}
+
